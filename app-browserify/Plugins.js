a464d01af2d4ee8f4172c968e66c365d3350c2cf
user7 <user7@mail.ru>
2015-12-22 13:45:47 +0200

    Добавлен функционал выбора и подсветки элемента списка фильтра.

---
+++ app-browserify/Plugins.js
@@ -0,0 +1,27 @@
+var $ = require('jquery');
+
+function highlight($this, $input) {
+	$this.
+	     find('[data-filter="' + $input.val() + '"]').
+	     addClass('data-active').
+	     siblings().
+	     removeClass('data-active');
+}
+
+var Plugins = {
+	setActiveStateAtList: function (elem) {
+		var $elem  = $(elem),
+		    $input = $elem.find('input');
+
+		highlight($elem, $input);
+
+		$elem.on('click', 'span', function () {
+			$input.val($(this).data('filter')).change();
+			highlight($elem, $input);
+		});
+
+		return $elem;
+	}
+};
+
+module.exports = Plugins;
--- app-browserify/views/dashboard/tasks/tasks_filters_view.js
+++ app-browserify/views/dashboard/tasks/tasks_filters_view.js
@@ -1,49 +1,51 @@
-var Backbone              = require('backbone'),
-    Helpers               = require('helpers'),
-    $                     = require('jquery'),
-    _                     = require('underscore'),
-    BaseView              = require('views/baseview'),
-    BaseListView          = require('views/elements/base_list_view'),
-    FilterModel           = require('models/dashboard/tasks_filter_model'),
-    tpl                   = require('templates/dashboard/tasks/tasks_filters.tpl');
+var Backbone     = require('backbone'),
+    Helpers      = require('helpers'),
+    Plugins      = require('plugins'),
+    $            = require('jquery'),
+    _            = require('underscore'),
+    BaseView     = require('views/baseview'),
+    BaseListView = require('views/elements/base_list_view'),
+    FilterModel  = require('models/dashboard/tasks_filter_model'),
+    tpl          = require('templates/dashboard/tasks/tasks_filters.tpl');
 
-var ContentView =  BaseView.extend({
+var ContentView = BaseView.extend({
 	template: tpl,
-    className : 'filters',
-    events: {
-    	'click .menu-item'   : 'changeFilter',
-        'change .priority'   : 'changePriority'
-    },
-    onInitialize: function(params) {
-        BaseView.prototype.onInitialize.call(this, params);
-        this.route = location.hash.split('?')[0] + '?';
+	className: 'filters',
+	events: {
+		'click .menu-item': 'changeFilter',
+		'change .priority': 'changePriority'
+	},
+	onInitialize: function (params) {
+		BaseView.prototype.onInitialize.call(this, params);
+		this.route = location.hash.split('?')[0] + '?';
 
-        this.model = new FilterModel({
-            priority: 'critical',
-            filter: 'all'
-        });
-        this.modelBinder = new Backbone.ModelBinder();
-        this.model.on('change', this.onModelChange, this);
-    },
-    onModelChange: function(){
-        Backbone.history.navigate(this.getRouteWithParams(), { trigger: true });
-    },
-    updateFilterModel: function(model){
-        this.model.set(model);
-    },
-    onRender: function() {
-        this.modelBinder.bind(this.model, this.el);
-    },
-    serialize: function(){
-        this.data = _.clone(this.model.attributes);
-    },
-    getRouteWithParams: function(){
-        return this.route + Helpers.serializeModel(this.model.toJSON());
-    },
-    remove : function () {
-        this.modelBinder.unbind();
-        BaseView.prototype.remove.call(this);
-    }
+		this.model = new FilterModel({
+			priority: 'critical',
+			filter: 'all'
+		});
+		this.modelBinder = new Backbone.ModelBinder();
+		this.model.on('change', this.onModelChange, this);
+	},
+	onModelChange: function () {
+		Backbone.history.navigate(this.getRouteWithParams(), {trigger: true});
+	},
+	updateFilterModel: function (model) {
+		this.model.set(model);
+	},
+	onRender: function () {
+		this.modelBinder.bind(this.model, this.el);
+		Plugins.setActiveStateAtList(this.getElement('.base-filters'));
+	},
+	serialize: function () {
+		this.data = _.clone(this.model.attributes);
+	},
+	getRouteWithParams: function () {
+		return this.route + Helpers.serializeModel(this.model.toJSON());
+	},
+	remove: function () {
+		this.modelBinder.unbind();
+		BaseView.prototype.remove.call(this);
+	}
 });
 
 module.exports = ContentView;
