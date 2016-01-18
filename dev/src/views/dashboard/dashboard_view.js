64b51795b3b3851131976b3365cee4b2882c3040
DeanNeal <venomsunset@gmail.com>
2016-01-18 10:20:04 +0200

    new tabs

--- dev/src/views/dashboard/dashboard_view.js
+++ dev/src/views/dashboard/dashboard_view.js
@@ -4,7 +4,6 @@ var RoutedView       = require('views/routedview');
 var BaseListView     = require('views/elements/base_list_view');
 var PulseView        = require('views/dashboard/pulse/dashboard_pulse_view');
 var tasksView        = require('views/dashboard/tasks/dashboard_tasks_view');
-// var milestonesView   = require('views/dashboard/milestones/dashboard_milestones_view');
 var projectsView     = require('views/dashboard/projects/dashboard_projects_view');
 var navBarCollection = require('collections/header_list');
 var mainTpl          = require('templates/dashboard/dashboard.tpl');
@@ -17,35 +16,43 @@ var ContentView = RoutedView.extend({
     routes: {
         'pulse'     : PulseView,
         'tasks'     : tasksView,
-        // 'milestones': milestonesView,
         'projects'  : projectsView
     },
     links: [{
         name: "Pulse",
-        id: "pulse",
-        iconClass: 'fa fa-exclamation-triangle'
+        route: "pulse",
+        iconClass: 'fa fa-exclamation-triangle',
+        prevent: true
     }, {
         name: "Tracker",
-        id: "tasks",
+        route: "dashboard/tasks",
         iconClass: 'fa fa-circle-o'
-    },/* {
-        name: "milestones",
-        id: "milestones",
-        iconClass: 'fa fa-circle-o'
-    },*/ {
+    },{
         name: "Projects",
-        id: "projects",
-        iconClass: 'fa fa-circle-o'
-    }],
+        route: "projects",
+        iconClass: 'fa fa-circle-o',
+        prevent: true
+    },{
+        name: "Timetable",
+        route: "timetable",
+        iconClass: 'fa fa-circle-o',
+        prevent: true
+    },
+    {
+        name: "Kanban",
+        route: "kanban",
+        iconClass: 'fa fa-circle-o',
+        prevent: true
+    }
+    ],
     onInitialize: function(params) {
         BaseView.prototype.onInitialize.call(this, params);
-        this.collection = Helpers.createLinks(this.links, params.stage);
         this.addView(BaseListView, {
             itemTpl: dashboardListTpl,
             //if not set then will get default classes
             // className: 'listClassName', //class name for <ul>
             // itemClassName: 'itemClassName', // class name for <li>
-            collection: new navBarCollection(this.collection)
+            collection: new navBarCollection(this.links)
         }, '.dashboard-header_menu');
     }
 });
