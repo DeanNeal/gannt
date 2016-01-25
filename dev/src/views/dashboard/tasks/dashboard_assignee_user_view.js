5e7f728a68c193a6051a8130c8d6a410104d2daf
DeanNeal <venomsunset@gmail.com>
2016-01-25 14:44:19 +0200

    assignee

--- dev/src/views/dashboard/tasks/dashboard_assignee_user_view.js
+++ dev/src/views/dashboard/tasks/dashboard_assignee_user_view.js
@@ -1,10 +1,31 @@
-var Backbone             = require('backbone'),
+var $                    = require('jquery'),
+	Backbone             = require('backbone'),
     BaseView             = require('views/baseview'),
     tpl                  = require('templates/dashboard/dashboard_assignee_user.tpl');
 
 var ContentView = BaseView.extend({
 	className: 'assignee-panel',
-	template: tpl
+	template: tpl,
+	events: {
+		'click .btn-apply'                : 'onApplyClick',
+		'click .assignee-panel_content li': 'onUserClick'
+	},
+	onInitialize: function (params) {
+		BaseView.prototype.onInitialize.call(this, params);
+	},
+	onApplyClick: function(){ 
+		this.parent.trigger('assignee:apply', this.text);
+	},
+	onUserClick: function(e){
+		var member = $(e.currentTarget),
+	        id = member.data('id'),
+			text = member.text(),
+			checkbox = $(e.currentTarget).find('input[type="checkbox"]');
+
+		this.text = text;
+		checkbox.prop('checked', !checkbox.is(':checked'));
+	}
+
 });
 
 module.exports = ContentView;
